85efa7e9e07417a91e254974001ad72993593fdd
ganatan <dannyganatan@gmail.com>
2024-06-21 09:17:41 +0200

    Update Angular 18.0.4 & Angular CLI 18.0.5

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,8 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet], templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
