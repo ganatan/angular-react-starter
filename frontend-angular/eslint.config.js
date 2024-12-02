f579b1af958cda9910452a3ef0e860defd441f7b
ganatan <dannyganatan@gmail.com>
2024-12-02 13:49:47 +0100

    Update Angular 19.0.1

--- frontend-angular/eslint.config.js
+++ frontend-angular/eslint.config.js
@@ -2,7 +2,7 @@
 const eslint = require("@eslint/js");
 const tseslint = require("typescript-eslint");
 const angular = require("angular-eslint");
-  
+
 module.exports = tseslint.config(
   {
     files: ["**/*.ts"],
--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet], templateUrl: './app.component.html',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
