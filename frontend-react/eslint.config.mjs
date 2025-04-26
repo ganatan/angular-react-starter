fd76056f7ef943814781b63dbe91a38af6a07178
ganatan <dannyganatan@gmail.com>
2025-04-26 08:10:51 +0200

    Fix tests & lint on frontend-react

--- frontend-react/eslint.config.mjs
+++ frontend-react/eslint.config.mjs
@@ -2,11 +2,22 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,jsx}"] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
