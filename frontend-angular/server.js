4cfb7a3cf7d3976ec94f3dbd833a2b34eba970f0
ganatan <dannyganatan@gmail.com>
2025-06-04 08:57:09 +0200

    fix serve express

--- frontend-angular/server.js
+++ frontend-angular/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist/angular-starter/browser')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist/angular-starter/browser', 'index.html'));
 });
 
-const port = 3000;
+const port = 4000;
 const host = 'localhost';
 app.listen(port, () => {
   console.log(`Server running at http://${host}:${port}`);
 })
+
